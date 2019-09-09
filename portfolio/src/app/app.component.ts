3c7cded22da943a968398e4cc3f3beb8ac80f3d5
ead <ead900@gmail.com>
2019-09-08 23:36:48 -0400

    scrap and renew angular project (ng new)

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-  	trigger('routerAnim', [
-  		state('entering', style({
-  			opacity:1
-  		})),
-  		state('leaving', style({
-  			opacity:0.4
-  		})),
-  		transition('* => *', animate('500ms'))
-  		])]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	routeState : string;
-	onLeaving() {
-		this.routeState = 'leaving';
-	}
-	onEntering() {
-		this.routeState = 'entering';
-	}
+  title = 'portfolio';
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,44 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore'; 
-import { environment } from '../environments/environment';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
-import { BlurbComponent } from './blurb/blurb.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes : Routes = [
-  { path: 'skills', component: BoardComponent },
-  { path: 'welcome', component: WelcomeComponent },
-  { path: '',
-    redirectTo: '/welcome',
-    pathMatch: 'full'
-  },
-  { path: '**', component: PageNotFoundComponent }
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardComponent,
-    BlurbComponent,
-    NavbarComponent,
-    WelcomeComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
